75e92afdeaca526749ccd48c0d6c03e68c9a8688
nuurhankhaled <nuurhankhaled@users.noreply.github.com>
2025-08-18 15:02:25 +0300

    adding new colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primaryColor = Color(0xFF25D366);
   static const secondaryColor = Color(0xFF128C7E);
+  static const tealColor = Color(0xFF1DAB61);
 
+  // Backgrounds:
+  static const iconBackground = Color(0xFFf7f7f7);
+  static const textFieldBackground = Color(0xFFF4F4F4);
   // Texts:
-  static const textPrimary = Color(0xFF020617);
-  static const textNavBar = Color(0xFF767779);
+  static const textPrimary = Color(0xFF0A0A0A);
+  static const textSecondary = Color(0xFF767779);
 
   static const error = Color(0xFFDC2626);
   static const border = Color(0xFF94A3B8);
