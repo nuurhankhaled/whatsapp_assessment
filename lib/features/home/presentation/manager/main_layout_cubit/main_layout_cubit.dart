5d23d40237b4dd6b847f099ea2321273622e6f26
Nuurhan Khaled <nuurhankhaled@gmail.com>
2025-08-20 00:26:55 +0300

    refactor the import

--- lib/features/home/presentation/manager/main_layout_cubit/main_layout_cubit.dart
+++ lib/features/home/presentation/manager/main_layout_cubit/main_layout_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:whatsapp_assessment/core/constant.dart';
+import 'package:whatsapp_assessment/core/constants/constant.dart';
 part 'main_layout_state.dart';
 
 class MainLayoutCubit extends Cubit<MainLayoutState> {
