fcf2f228ce754ee69e1276c264e3fefde892b407
Nuurhan Khaled <nuurhankhaled@gmail.com>
2025-08-21 14:57:23 +0300

    adding provider to the app

--- lib/whatsapp_app.dart
+++ lib/whatsapp_app.dart
@@ -1,30 +1,49 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whatsapp_assessment/core/cache_helper/cache_helper.dart';
+import 'package:whatsapp_assessment/core/cache_helper/cache_values.dart';
 import 'package:whatsapp_assessment/core/routing/router.dart';
 import 'package:whatsapp_assessment/core/theme/app_theme.dart';
+import 'package:whatsapp_assessment/features/home/presentation/manager/change_mood_cubit/change_mood_cubit.dart';
 import 'package:whatsapp_assessment/generated/translations/locale_keys.g.dart';
 
 class WhatsAppApp extends StatelessWidget {
   WhatsAppApp({super.key});
+  bool isdark = CacheHelper.getData(key: CacheKeys.isDark) as bool? ?? false;
 
   final appRouter = RootRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: tr(LocaleKeys.app_name),
-      builder: (context, child) {
-        return MediaQuery.withNoTextScaling(child: child ?? const SizedBox());
-      },
-      debugShowCheckedModeBanner: false,
-      locale: context.locale,
-      onGenerateTitle: (_) => tr(LocaleKeys.app_name),
-      supportedLocales: context.supportedLocales,
-      localizationsDelegates: context.localizationDelegates,
-      routeInformationParser: appRouter.defaultRouteParser(),
-      routerDelegate: AutoRouterDelegate(appRouter),
-      theme: AppTheme.instance.lightTheme,
+    return BlocProvider(
+      create: (context) => ChangeMoodCubit()..changeTheme(fromShared: isdark),
+      child: BlocConsumer<ChangeMoodCubit, ChangeMoodStates>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          return MaterialApp.router(
+            title: tr(LocaleKeys.app_name),
+            builder: (context, child) {
+              return MediaQuery.withNoTextScaling(
+                child: child ?? const SizedBox(),
+              );
+            },
+            debugShowCheckedModeBanner: false,
+            locale: context.locale,
+            onGenerateTitle: (_) => tr(LocaleKeys.app_name),
+            supportedLocales: context.supportedLocales,
+            localizationsDelegates: context.localizationDelegates,
+            routeInformationParser: appRouter.defaultRouteParser(),
+            routerDelegate: AutoRouterDelegate(appRouter),
+            theme: AppTheme.instance.lightTheme,
+            darkTheme: AppTheme.instance.darkTheme,
+            themeMode: isdark ? ThemeMode.dark : ThemeMode.light,
+          );
+        },
+      ),
     );
   }
 }
