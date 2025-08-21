34924e2845078e59870561642155fa84d6f2f8ec
Nuurhan Khaled <nuurhankhaled@gmail.com>
2025-08-21 14:57:04 +0300

    adding cache helpers and cache keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:whatsapp_assessment/core/cache_helper/cache_helper.dart';
 import 'package:whatsapp_assessment/core/constants/localization.dart';
 import 'package:whatsapp_assessment/whatsapp_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await CacheHelper.init();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
